6d2f8e715442b282051ebd04e7cf1cdf85aa0e28
Vladislav Sidorov <joncan4sda@yandex.ru>
2018-09-05 21:52:16 +0400

    add WebService

---
+++ WebServise/MapDAL/MapDALConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace MapDAL
+{
+    public class MapDALConfig: ConfigurationSection
+    {
+        public const string SectionName = "MapDALConfig";
+
+        private const string ConnectionNameField = "MapDALConnectionName";
+
+        public MapDALConfig()
+        {
+
+        }
+
+        [ConfigurationProperty(ConnectionNameField, IsRequired = true)]
+        public string ConnectionName
+        {
+            get
+            {
+                return (string)this[ConnectionNameField];
+            }
+            set
+            {
+                this[ConnectionNameField] = value;
+            }
+        }
+    }
+}
---
+++ WebServise/WebService/Controllers/LayersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebService.Controllers
+{
+    public class LayersController : ApiController
+    {
+        // GET: api/Layers
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Layers/5
+        public string Get(string layerId)
+        {
+            return "value";
+        }
+    }
+}
